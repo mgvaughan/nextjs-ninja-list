7e2359f8b2383c96a7b3aa00786493e46f4d39c9
Michael Vaughan <mgvaughan66@gmail.com>
2023-01-14 17:47:38 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+        <Link className={styles.btn} href="/ninjas">
+          See Ninja Listing
+        </Link>
+      </div>
+    </>
+  );
+}
